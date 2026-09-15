8eea11b8449fceac4059c30ec567d36580c1b2bd
whoami <85992977+whoamizy@users.noreply.github.com>
2026-09-15 09:57:12 +0400

    Stage C (#3)
    * user input stage 1
    
    * user input stage 2
    
    * user input stage 3
    
    * user input stage 4
    
    * user input stage 5
    
    * user input stage 6
    
    ---------
    
    Co-authored-by: Sergei Danilov <s.danilov@klerk.ru>

--- main.go
+++ main.go
@@ -1,16 +1,122 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"task-cli/task"
 )
 
+var COMMANDS = map[string]string{
+	"add":    "Добавить новую задачу",
+	"list":   "Показать список всех задач",
+	"done":   "Отметить задачу как выполненную",
+	"delete": "Удалить задачу",
+	"find":   "Найти задачи по тексту",
+	"help":   "Показать список доступных команд",
+	"exit":   "Завершить программу",
+}
+
 func main() {
-	task := task.NewTask(1, "Learn Go")
+	manager := task.NewManager()
 
-	fmt.Println(task)
+	scanner := bufio.NewScanner(os.Stdin)
 
-	task.Complete()
+	for {
+		fmt.Print("Введите команду: ")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Println("Ошибка ввода:", err)
+			}
+			return
+		}
 
-	fmt.Println(task)
+		input := scanner.Text()
+		parts := strings.Fields(input)
+		if len(parts) == 0 {
+			fmt.Println("Пустая команда, попробуйте другую")
+			continue
+		}
+		cmd := parts[0]
+
+		switch cmd {
+		case "find":
+			if len(parts) < 2 {
+				fmt.Println("Укажи Title задачи")
+				continue
+			}
+			query := strings.Join(parts[1:], " ")
+			res := manager.Find(query)
+			if len(res) == 0 {
+				fmt.Println("Ничего не найдено")
+				continue
+			}
+			for _, task := range res {
+				fmt.Println(task)
+			}
+		case "delete":
+			if len(parts) < 2 {
+				fmt.Println("Укажи ID задачи")
+				continue
+			}
+			taskId, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Println("Некорректный ID")
+				continue
+			}
+			err = manager.Delete(taskId)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("Задача удалена")
+		case "done":
+			if len(parts) < 2 {
+				fmt.Println("Укажи ID задачи")
+				continue
+			}
+			taskId, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Println("Некорректный ID")
+				continue
+			}
+			err = manager.Complete(taskId)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("Задача завершена")
+		case "add":
+			userTask := parts[1:]
+			if len(userTask) == 0 {
+				fmt.Println("Такое нельзя добавить")
+				continue
+			}
+			task := manager.Add(strings.Join(userTask, " "))
+
+			fmt.Println("Добавлена задача:")
+			fmt.Println(task)
+		case "help":
+			for key, value := range COMMANDS {
+				fmt.Println(key, "-", value)
+			}
+		case "list":
+			tasks := manager.List()
+			if len(tasks) == 0 {
+				fmt.Println("Список пуст")
+				continue
+			}
+			for _, task := range tasks {
+				fmt.Println(task)
+			}
+		case "exit":
+			fmt.Println("До скорого!")
+			return
+		default:
+			fmt.Println("Такой команды нет, попробуйте другую")
+			continue
+		}
+	}
 }
